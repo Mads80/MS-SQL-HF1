bcecbccbd372d6dc1a03a5e47098e89615b05fee
Jona525r <65536651+Jona525r@users.noreply.github.com>
2020-05-18 14:10:54 +0200

    Add files via upload

--- Sportsvognklubben-post.sql
+++ Sportsvognklubben-post.sql
@@ -1,26 +1,4 @@
---HEJ <3
-USE master
 
---Først opretter vi en database
-CREATE DATABASE Sportsvognklub;
-GO
---Bruger den nyoprettede database
-USE Sportsvognklub;
-
---Opretter en tabel med navnet "Klubinfo"
-CREATE TABLE Klubinfo (
-	ID int IDENTITY (1,1) PRIMARY KEY,
-	Fornavn varchar(55) NOT NULL,
-	Efternavn varchar(55) NOT NULL,
-	Adresse varchar(55) NOT NULL,
-	Telefonnummer int NOT NULL,
-	Postnummer int NOT NULL,
-	Medlemsnummer int NOT NULL,
-	Indmeldingsdato varchar(55) NOT NULL,
-	Biltype varchar(55) NOT NULL,
-	Bilensaargang varchar(55) NOT NULL,
-	Andeninfo varchar(55)
-);
 -- Opretter en tebel med navnet "Data_Postnumre"
 CREATE TABLE Data_Postnumre (
   id integer PRIMARY KEY NOT NULL,
@@ -1455,11 +1433,3 @@ INSERT INTO Data_Postnumre VALUES
 	(1417,'Region Sjælland','Vordingborg Kommune',4791,'Borre'),
 	(1418,'Region Sjælland','Vordingborg Kommune',4792,'Askeby'),
 	(1419,'Region Sjælland','Vordingborg Kommune',4793,'Bogø By');
-
-SELECT * FROM Data_Postnumre
---Indsætter data i tabellen "Klubinfo"
-INSERT INTO Klubinfo VALUES ('Mark','Pedersen','Hanborienhule','666','5000','1','18-05-2020','Fedøse','1845','Andeninfokommerher')
-
-
-SELECT * FROM Klubinfo;
-
