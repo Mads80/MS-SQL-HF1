b0a0dcded1912f79701a1d4000ce871eb8308eaf
Mads80 <deef.deef@gmail.com>
2020-05-18 11:30:20 +0200

    Update Sportsvognklubben.sql

--- Sportsvognklubben.sql
+++ Sportsvognklubben.sql
@@ -1,6 +1,20 @@
 --HEJ <3
 USE master
 
-CREATE DATABASE Sportsvogne;
+CREATE DATABASE Sportsvognklub;
 GO
-USE Sportsvogne;
+USE Sportsvognklub;
+
+CREATE TABLE Klubinfo (
+	ID int IDENTITY (1,1) PRIMARY KEY,
+	Fornavn varchar(55) NOT NULL,
+	Efternavn varchar(55) NOT NULL,
+	Adresse varchar(55) NOT NULL,
+	Telefonnummer int NOT NULL,
+	Postnummer int NOT NULL,
+	Medlemsnummer int NOT NULL,
+	Indmeldingsdato varchar(55) NOT NULL,
+	Biltype varchar(55) NOT NULL,
+	Bilensaargang varchar(55) NOT NULL,
+	Andeninfo varchar(55)
+);
